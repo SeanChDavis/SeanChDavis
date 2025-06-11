bd718bf045cb9d6889de429e141b97d861310c1c
SeanChDavis <seanchdavis@gmail.com>
2025-06-11 14:39:43 -0500

    remove footer border

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -5,7 +5,7 @@ import siteData from "../../data/siteData.json";
 const Footer = () => {
     return (
         <footer
-          className="bg-gray-100 dark:bg-gray-950 border-t border-gray-100 dark:border-gray-950 py-6 px-4 md:px-8 text-sm text-center text-gray-600 dark:text-gray-400 font-semibold">
+          className="bg-gray-100 dark:bg-gray-950 py-6 px-4 md:px-8 text-sm text-center text-gray-600 dark:text-gray-400 font-semibold">
           &copy; {new Date().getFullYear()} <Link className="dark:text-gray-200 dark:hover:transition dark:hover:duration-300 dark:hover:ease-in-out dark:hover:text-gray-400"
                                                   to="/">{siteData.siteNameShort}</Link> &middot; {siteData.footerText}
         </footer>
