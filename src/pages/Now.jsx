9fa676f592624ae541068a04da98a7f05cf16834
SeanChDavis <seanchdavis@gmail.com>
2025-06-14 10:01:48 -0500

    fix page section spacing

--- src/pages/Now.jsx
+++ src/pages/Now.jsx
@@ -8,27 +8,27 @@ export default function Now({ linkClasses, headingClasses, mainHeadingClasses })
             <h1 className={mainHeadingClasses}>What I'm Doing Now</h1>
             <p>This is a snapshot of where my focus is right now — last updated May 2025.</p>
             <div className="space-y-6">
-                <div>
+                <div className="space-y-2">
                     <h2 className={headingClasses}>BS in Information Systems — Completed</h2>
                     <p>I recently completed a bachelor's degree at <abbr className="cursor-help no-underline" title="University of Missouri-St. Louis">UMSL</abbr>. No plans for graduate school — I'm focused on getting back to applying what I know.</p>
                 </div>
 
-                <div>
+                <div className="space-y-2">
                     <h2 className={headingClasses}>Preparing for the Netherlands</h2>
                     <p>My plan for moving to the Netherlands is in motion. I’m targeting a September 2025 <Link to="/netherlands-journey" className={linkClasses}>relocation to Rotterdam, NL</Link> using the <abbr className="cursor-help no-underline" title="Dutch American Friendship Treaty">DAFT</abbr> visa.</p>
                 </div>
 
-                <div>
+                <div className="space-y-2">
                     <h2 className={headingClasses}>Living light, staying agile</h2>
                     <p>I’ve reduced digital and real-world clutter with a goal of operating efficiently and with the ability to adjust when needed. I am always on the move in one way or another. Join, but do not impede.</p>
                 </div>
 
-                <div>
+                <div className="space-y-2">
                     <h2 className={headingClasses}>Evolving SeanChDavis.com</h2>
                     <p>After nearly two decades of juggling multiple domain names, email addresses, and web platforms, I've decided to build a single platform tailored to my needs. You are here. More to come.</p>
                 </div>
 
-                <div>
+                <div className="space-y-2">
                     <h2 className={headingClasses}>Personal system design</h2>
                     <p>We have all created and lived by some sort of life management system. It's how we do things. It's how we survive. I am intentionally designing mine with expectations of specific output.</p>
                 </div>
